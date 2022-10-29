2fb007aa481981c9c1aa94858de5a00ce266a552
João Emanuel da Silva Mendonça <ggg2j15@gmail.com>
2022-10-29 20:32:42 -0300

    Mongo database test

--- tests/database/no_relational/mongo_database_test.py
+++ tests/database/no_relational/mongo_database_test.py
@@ -1,7 +1,62 @@
 from sys import path
 path.append('..')
 
-from mlc.source import mongo
+from mlc.source import mongo, ListModel
 
 def test_answer():
-    pass
+    id_user = 1
+
+    # Insert
+    ## Valid
+
+    data = {
+        'id_user': id_user,
+        'data': ['1', '2', '3']
+    }
+    result = mongo.insert_one(ListModel, data)
+    assert result == True
+
+    ## Invalid
+
+    data = {
+        'id_user': id_user,
+        'list': [132]
+    }
+    result = mongo.insert_one(ListModel, data)
+    assert result == False
+
+    # Select
+
+    ## Valid
+    data = {
+        'id_user': id_user
+    }
+    result = mongo.select(ListModel, data)
+    _id = result[0]['_id']
+    assert result != []
+
+    ## Invalid
+    data = {
+        'id_user': -1000
+    }
+    result = mongo.select(ListModel, data)
+    assert result == []
+
+    # Update
+
+    ## Valid
+    data = {
+        'data': ['1', '3', '2']        
+    }
+    result = mongo.update_one(_id, ListModel, data)
+    assert result == True
+
+    ## Invalid 
+
+    result = mongo.update_one(-1, ListModel, data)
+    assert result == False
+
+    # Delete
+
+    result = mongo.delete_one(_id, ListModel)
+    assert result == True
