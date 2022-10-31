9468694451f57686cb9ffadfd71dce100a2b6241
João Emanuel da Silva Mendonça <ggg2j15@gmail.com>
2022-10-30 21:25:17 -0300

    api > api test hash

--- tests/routes/api/api_test.py
+++ tests/routes/api/api_test.py
@@ -1,4 +1,4 @@
-from requests import get
+from requests import get, post
 
 def test_answer():
     api_url = 'http://127.0.0.1:8080/api'
@@ -8,3 +8,48 @@ def test_answer():
     response = get(api_url).json()
     assert type(response) == dict
     assert response['Hello'] == 'World'
+
+    # Hash
+
+    ## Generate
+
+    new_hash_url = f'{api_url}/hash/new_hash'
+
+    ### Valid
+
+    data = {'text': 'Hello World'}
+    response = post(
+        url=new_hash_url,
+        data=data
+    ).json()
+    assert type(response) == dict
+    assert response['hash'][0:10] == 'a591a6d40b'
+    full_hash = response['hash']
+
+    ### Invalid
+    invalid_data = {'hash': 'Hello World'}
+    response = post(
+        url=new_hash_url,
+        data=invalid_data
+    ).json()
+    assert response['hash'] == 'Error Invalid query parameter'
+
+    ## Compare
+
+    compare_hash_url = f'{api_url}/hash/compare_hash'
+
+    ### Valid
+    data['hash'] = full_hash
+    response = post(
+        url=compare_hash_url,
+        data=data
+    ).json()
+    assert type(response) == dict
+    assert response['equal'] == True
+
+    ### Invalid
+    response = post(
+        url=compare_hash_url,
+        data=invalid_data
+    ).json()
+    assert response['equal'] == False
