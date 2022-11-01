dca4c844448adb8bd16f22b34ab58b3f2c0ccb42
João Emanuel da Silva Mendonça <ggg2j15@gmail.com>
2022-11-01 10:28:29 -0300

    tests > hash_test.py

---
+++ tests/source/hash_test.py
@@ -0,0 +1,14 @@
+from sys import path
+
+path.append('..')
+
+from mlc.source import Factory, HashInterface
+
+def test_answer():
+    fac = Factory()
+    hash: HashInterface = fac.get_representative(HashInterface)()
+    assert isinstance(hash, HashInterface)
+
+    text = 'Hello World'
+    hash_text = hash.generate_hash(text)
+    assert hash.compare_hash(text, hash_text)
