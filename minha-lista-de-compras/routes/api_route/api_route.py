cac28ed945d024a2072d82e1b28c28ffd1154350
João Emanuel da Silva Mendonça <ggg2j15@gmail.com>
2022-11-01 10:03:25 -0300

    Update api_route.py

--- minha-lista-de-compras/routes/api_route/api_route.py
+++ minha-lista-de-compras/routes/api_route/api_route.py
@@ -1,12 +1,13 @@
 from flask import Blueprint, jsonify
 
-from .routes_class import hash_sub_route
+from .routes_class import hash_sub_route, encrypt_sub_route
 
 api = Blueprint('api', __name__)
 
 # Sub routes
 
 api.register_blueprint(hash_sub_route, url_prefix='/hash')
+api.register_blueprint(encrypt_sub_route, url_prefix='/encrypt')
 
 @api.route('/')
 def api_index():
