814b920cd8e4b84aa8f90b8ab9cff163dfe9df65
João Emanuel da Silva Mendonça <ggg2j15@gmail.com>
2022-11-04 11:00:27 -0300

    database > sql_lite_database_interface.py

--- minha-lista-de-compras/mlc_dir/source/database/relational/interfaces/sql_lite_database_interface.py
+++ minha-lista-de-compras/mlc_dir/source/database/relational/interfaces/sql_lite_database_interface.py
@@ -1,31 +1,20 @@
 from abc import abstractmethod
 from typing import Type, List, Dict, Any, Dict, Union
 
-from orm_sqlite import Database, Model
+from mlc_dir import sql_db as db
 
 from .database_interface import DatabaseInterface
 
 class SQLiteDatabaseInterface(DatabaseInterface):
     """Sql lite database interface"""
-    @abstractmethod
-    def create_connection(self, database_path: str) -> Type[Database]:
-        """Create a connection from db file
-
-        Args:
-            database_path (str): path to db file
-
-        Returns:
-            Type[Database]: orm_sqlite Database object
-        """        
-        raise NotImplementedError()
 
     @abstractmethod
-    def select(self, model: Type[Model]=None, connection: Type[Database]=None, fields: List[str]=[], where:Dict[str, Any]=None) -> Union[List[Dict[str, Any]], list]:
+    def select(self, model: Type[db.Model]=None, connection: Type[db]=None, fields: List[str]=[], where:Dict[str, Any]=None) -> Union[List[Dict[str, Any]], list]:
         """Select [fields] in [model] [where]
 
         Args:
-            model (Type[Model]): Model to realize a select. Defaults to None.
-            connection (Type[Database]): Connection. Defaults to None.
+            model (Type[db.Model]): Model to realize a select. Defaults to None.
+            connection (Type[db]): Connection. Defaults to None.
             fields (List[str]): Fields, if '*' then all fields will be returned. Defaults to [].
             where (Dict[str, Any]): Where, if '*' then all records will be returned. Defaults to None.
 
@@ -35,14 +24,14 @@ class SQLiteDatabaseInterface(DatabaseInterface):
         raise NotImplementedError()
 
     @abstractmethod
-    def update_one(self, id: int=None, model: Type[Model]=None, data: Dict[str, Any]=None, connection: Type[Database]=None) -> bool:
+    def update_one(self, id: int=None, model: Type[db.Model]=None, data: Dict[str, Any]=None, connection: Type[db]=None) -> bool:
         """Update [model] set [data] where [id = id]
 
         Args:
             id (int): id to record. Defaults to None.
-            model (Type[Model]): Model to realize a update. Defaults to None.
+            model (Type[db.Model]): Model to realize a update. Defaults to None.
             data (Dict[str, Any]): Data to realize a update. Defaults to None.
-            connection (Type[Database]): Connection. Defaults to None.
+            connection (Type[db]): Connection. Defaults to None.
 
         Returns:
             bool: True if successfully, else False
@@ -50,13 +39,13 @@ class SQLiteDatabaseInterface(DatabaseInterface):
         raise NotImplementedError()
 
     @abstractmethod
-    def insert_one(self, model: Type[Model]=None, data: Dict[str, Any]=None, connection: Type[Database]=None) -> bool:
+    def insert_one(self, model: Type[db.Model]=None, data: Dict[str, Any]=None, connection: Type[db]=None) -> bool:
         """insert into [model] values [data]
 
         Args:
-            model (Type[Model]): Model to realize a insert. Defaults to None.
+            model (Type[db.Model]): Model to realize a insert. Defaults to None.
             data (Dict[str, Any]): Data to realize a insert. Defaults to None.
-            connection (Type[Database]): Connection. Defaults to None.
+            connection (Type[db]): Connection. Defaults to None.
 
         Returns:
             bool: True if successfully, else False
@@ -64,13 +53,13 @@ class SQLiteDatabaseInterface(DatabaseInterface):
         raise NotImplementedError()
 
     @abstractmethod
-    def delete_one(self, id: int=None, model: Type[Model]=None, connection: Type[Database]=None) -> bool:
+    def delete_one(self, id: int=None, model: Type[db.Model]=None, connection: Type[db]=None) -> bool:
         """delete from [model] where [id = id]
 
         Args:
             id (int): id to record. Defaults to None.
-            model (Type[Model]): Model to realize a delete. Defaults to None.
-            connection (Type[Database]): Connection. Defaults to None.
+            model (Type[db.Model]): Model to realize a delete. Defaults to None.
+            connection (Type[db]): Connection. Defaults to None.
 
         Returns:
             bool: True if successfully, else False
